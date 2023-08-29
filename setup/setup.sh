89c1bd172ab1beebf212a1f639e1b3af05c9b2fd
arne <arne@apache.org>
2023-08-29 13:48:53 +0200

    feat: Create gogs user on setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,1 +1,10 @@
-echo "Hello Setup"
+echo "Creating Gogs user..."
+curl \
+ --retry 20 \
+ -f \
+ --retry-all-errors \
+ --retry-delay 30 \
+ -o /dev/null \
+ -d "user_name=openknowledge&email=test@openknowledge.de&password=workshop&retype=workshop" \
+ -X POST http://gogs-service:3000/user/sign_up
+echo "Gogs user created."
