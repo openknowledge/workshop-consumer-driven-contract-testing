5a42b74439a4f032ca7379e45eeb7e939f45652d
Arne Limburg <arne.limburg@openknowledge.de>
2023-08-29 18:40:42 +0200

    feat: Add git repositories on setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,7 +4,36 @@ curl \
  -f \
  --retry-all-errors \
  --retry-delay 30 \
- -o /dev/null \
  -d "user_name=openknowledge&email=test@openknowledge.de&password=workshop&retype=workshop" \
  -X POST http://gogs-service:3000/user/sign_up
 echo "Gogs user created."
+
+echo "Creating repositories..."
+
+curl \
+ -f \
+ -u "openknowledge:workshop" \
+ -d "name=customer-service" \
+ -X POST http://gogs-service:3000/api/v1/admin/users/openknowledge/repos
+echo "Repository 'customer-service' created."
+
+curl \
+ -f \
+ -u "openknowledge:workshop" \
+ -d "name=billing-service" \
+ -X POST http://gogs-service:3000/api/v1/admin/users/openknowledge/repos
+echo "Repository 'billing-service' created."
+
+curl \
+ -f \
+ -u "openknowledge:workshop" \
+ -d "name=delivery-service" \
+ -X POST http://gogs-service:3000/api/v1/admin/users/openknowledge/repos
+echo "Repository 'delivery-service' created."
+
+curl \
+ -f \
+ -u "openknowledge:workshop" \
+ -d "name=address-validation-service" \
+ -X POST http://gogs-service:3000/api/v1/admin/users/openknowledge/repos
+echo "Repository 'address-validation-service' created."
