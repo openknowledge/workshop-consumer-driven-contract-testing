e5cbf00f7a241ef7dfeab0a080e136e0bf330065
hendrik <hendrik.mueller@openknowledge.de>
2023-09-15 13:45:15 +0200

    fix: clean up

--- scripts/jenkinsCurl.sh
+++ scripts/jenkinsCurl.sh
@@ -6,7 +6,7 @@ TMP_DIR=.tmp/jenkins
 mkdir -p $TMP_DIR
 cd $TMP_DIR
 
-JENKINS_URL="http://localhost:30070"
+JENKINS_URL="http://localhost:30040"
 
 COOKIE_FILE=./cookiefile
 CRUMB=$(curl -c $COOKIE_FILE --silent "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
