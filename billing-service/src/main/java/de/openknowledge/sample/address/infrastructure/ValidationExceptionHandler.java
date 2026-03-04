openknowledge/workshop-api-design

feat: Add consumer driven contract testing

billing-service/src/main/java/de/openknowledge/sample/address/infrastructure/ValidationExceptionHandler.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright 2019 open knowledge GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.openknowledge.sample.address.infrastructure;
+
+import javax.validation.ValidationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ValidationExceptionHandler implements ExceptionMapper<ValidationException> {
+
+    private static final String PROBLEM_JSON_TYPE = "application/problem+json";
+    private static final String PROBLEM_JSON
+        = "{\"type\": \"%s\", \"title\": \"%s\", \"status\": %d, \"detail\": \"%s\", \"instance\": \"%s\"}";
+
+    @Context
+    private UriInfo uri;
+
+    @Override
+    public Response toResponse(ValidationException exception) {
+        return Response.status(Response.Status.BAD_REQUEST)
+                .type(PROBLEM_JSON_TYPE)
+                .entity(String.format(
+                        PROBLEM_JSON,
+                        uri.getBaseUri().resolve("/errors/invalid-" + uri.getPathSegments().get(uri.getPathSegments().size() - 1)),
+                        "bad request", Response.Status.BAD_REQUEST.getStatusCode(),
+                        exception.getMessage(),
+                        uri.getAbsolutePath().toString()))
+                .build();
+    }
+}

customer-service/src/test/java/de/openknowledge/sample/address/domain/BillingAddressRepositoryTest.java
@@ -1,0 +1,130 @@
+/*
+ * Copyright 2019 open knowledge GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.openknowledge.sample.address.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.ws.rs.client.ClientBuilder;
+
+import org.apache.johnzon.jaxrs.jsonb.jaxrs.JsonbJaxrsProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.PactSpecVersion;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import de.openknowledge.sample.customer.domain.CustomerNumber;
+
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(providerName = "billing-service", pactVersion = PactSpecVersion.V3)
+public class BillingAddressRepositoryTest {
+
+    private BillingAddressRepository repository;
+
+    @Pact(consumer = "customer-service")
+    public RequestResponsePact getMax(PactDslWithProvider builder) throws IOException {
+        return builder
+          .given("Three customers")
+            .uponReceiving("GET request for 0815")
+            .path("/billing-addresses/0815")
+            .method("GET")
+          .willRespondWith()
+            .status(200)
+            .body(new PactDslJsonBody()
+                    .stringValue("recipient", "Max Mustermann")
+                    .stringValue("city", "26122 Oldenburg")
+                    .object("street")
+                        .stringValue("name", "Poststr.")
+                        .stringValue("number", "1")
+                    .closeObject())
+          .toPact();
+    }
+
+    @Pact(consumer = "customer-service")
+    public RequestResponsePact dontGetMissing(PactDslWithProvider builder) throws IOException {
+        return builder
+          .given("Three customers")
+            .uponReceiving("GET request for 0817")
+            .path("/billing-addresses/0817")
+            .method("GET")
+          .willRespondWith()
+            .status(404)
+          .toPact();
+    }
+
+    @Pact(consumer = "customer-service")
+    public RequestResponsePact updateMax(PactDslWithProvider builder) throws IOException {
+        return builder
+          .given("Three customers")
+            .uponReceiving("POST request for 0815")
+            .path("/billing-addresses/0815")
+            .method("POST")
+            .matchHeader("Content-Type", "application/json.*", "application/json")
+            .body(new PactDslJsonBody()
+                    .stringValue("recipient", "Erika Mustermann")
+                    .stringValue("city", "45127 Essen")
+                    .object("street")
+                        .stringValue("name", "II. Hagen")
+                        .stringValue("number", "7")
+                    .closeObject())
+          .willRespondWith()
+            .status(200)
+          .toPact();
+    }
+
+    @BeforeEach
+    public void initializeRepository(MockServer mockServer) {
+        repository = new BillingAddressRepository();
+        repository.billingServiceUrl = "http://localhost:" + mockServer.getPort();
+        repository.client = ClientBuilder.newClient().register(JsonbJaxrsProvider.class);
+    }
+
+    @PactTestFor(pactMethod = "getMax")
+    @Test
+    public void findDeliveryAddressForExistingCustomer() {
+        Optional<Address> address = repository.find(new CustomerNumber("0815"));
+        assertThat(address).isPresent().contains(
+            new Address(
+                new Recipient("Max Mustermann"),
+                new Street(new StreetName("Poststr."), new HouseNumber("1")),
+                new City("26122 Oldenburg")));
+    }
+
+    @PactTestFor(pactMethod = "dontGetMissing")
+    @Test
+    public void dontFindNonExistingAddress() {
+        Optional<Address> address = repository.find(new CustomerNumber("0817"));
+        assertThat(address).isNotPresent();
+    }
+
+    @PactTestFor(pactMethod = "updateMax")
+    @Test
+    public void updateAddress() {
+        repository.update(new CustomerNumber("0815"), new Address(
+            new Recipient("Erika Mustermann"),
+            new Street(new StreetName("II. Hagen"), new HouseNumber("7")),
+            new City("45127 Essen")));
+    }
+}

delivery-service/src/main/java/de/openknowledge/sample/address/domain/Address.java
@@ -15,18 +15,38 @@
  */
 package de.openknowledge.sample.address.domain;
 
-import static java.util.Optional.ofNullable;
 import static org.apache.commons.lang3.Validate.notNull;
-
-import java.util.Objects;
 
 import javax.json.bind.annotation.JsonbCreator;
 import javax.json.bind.annotation.JsonbProperty;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "ADDRESSES")
 public class Address {
+
+    @EmbeddedId
+    CustomerNumber id;
+    @Embedded
     private Recipient recipient;
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "name.name", column = @Column(name = "STREET")),
+        @AttributeOverride(name = "number.number", column = @Column(name = "HOUSE_NUMBER")),
+    })
     private Street street;
+    @Embedded
     private City city;
+
+    protected Address() {
+        // for JPA
+    }
 
     @JsonbCreator
     public Address(@JsonbProperty("recipient") Recipient recipient) {
@@ -57,50 +77,5 @@ public class Address {
 
     public void setCity(City city) {
         this.city = city;
-    }
-
-    @Override
-    public int hashCode() {
-        return recipient.hashCode() + ofNullable(street).map(Object::hashCode).orElse(0) + ofNullable(city).map(Object::hashCode).orElse(0);
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || !getClass().equals(object.getClass())) {
-            return false;
-        }
-        Address address = (Address)object;
-        return recipient.equals(recipient) && Objects.equals(street, address.street) && Objects.equals(city, address.city);
-    }
-
-    public static Builder of(String recipient) {
-        return new Builder(new Recipient(recipient));
-    }
-
-    public static class Builder {
-
-        private Address address;
-
-        private Builder(Recipient recipient) {
-            address = new Address(recipient);
-        }
-
-        public Builder atStreet(String name) {
-            AddressLine addressLine = new AddressLine(name);
-            address.setStreet(new Street(addressLine.getStreetName(), addressLine.getHouseNumber()));
-            return this;
-        }
-
-        public Builder inCity(String city) {
-            address.setCity(new City(city));
-            return this;
-        }
-
-        public Address build() {
-            return address;
-        }
     }
 }

delivery-service/src/main/java/de/openknowledge/sample/address/domain/CustomerNumber.java
@@ -17,14 +17,20 @@ package de.openknowledge.sample.address.domain;
 
 import static org.apache.commons.lang3.Validate.notBlank;
 
+import java.io.Serializable;
+
 import javax.json.bind.adapter.JsonbAdapter;
 import javax.json.bind.annotation.JsonbTypeAdapter;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 
 import de.openknowledge.sample.address.domain.CustomerNumber.Adapter;
 
+@Embeddable
 @JsonbTypeAdapter(Adapter.class)
-public class CustomerNumber {
+public class CustomerNumber implements Serializable {
 
+    @Column(name = "ID")
     private String number;
 
     protected CustomerNumber() {

delivery-service/src/main/java/de/openknowledge/sample/address/domain/StreetName.java
@@ -19,9 +19,11 @@ import static org.apache.commons.lang3.Validate.notBlank;
 
 import javax.json.bind.adapter.JsonbAdapter;
 import javax.json.bind.annotation.JsonbTypeAdapter;
+import javax.persistence.Embeddable;
 
 import de.openknowledge.sample.address.domain.StreetName.Adapter;
 
+@Embeddable
 @JsonbTypeAdapter(Adapter.class)
 public class StreetName {
 

openknowledge/workshop-consumer-driven-contract-testing

billing-service/src/main/java/de/openknowledge/sample/address/infrastructure/ValidationExceptionHandler.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright 2019 open knowledge GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.openknowledge.sample.address.infrastructure;
+
+import jakarta.validation.ValidationException;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class ValidationExceptionHandler implements ExceptionMapper<ValidationException> {
+
+    private static final String PROBLEM_JSON_TYPE = "application/problem+json";
+    private static final String PROBLEM_JSON
+        = "{\"type\": \"%s\", \"title\": \"%s\", \"status\": %d, \"detail\": \"%s\", \"instance\": \"%s\"}";
+
+    @Context
+    private UriInfo uri;
+
+    @Override
+    public Response toResponse(ValidationException exception) {
+        return Response.status(Response.Status.BAD_REQUEST)
+                .type(PROBLEM_JSON_TYPE)
+                .entity(String.format(
+                        PROBLEM_JSON,
+                        uri.getBaseUri().resolve("/errors/invalid-" + uri.getPathSegments().get(uri.getPathSegments().size() - 1)),
+                        "bad request", Response.Status.BAD_REQUEST.getStatusCode(),
+                        exception.getMessage(),
+                        uri.getAbsolutePath().toString()))
+                .build();
+    }
+}

customer-service/src/test/java/de/openknowledge/sample/address/domain/BillingAddressRepositoryTest.java
@@ -1,0 +1,129 @@
+/*
+ * Copyright 2019 open knowledge GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.openknowledge.sample.address.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import jakarta.ws.rs.client.ClientBuilder;
+
+import org.apache.johnzon.jaxrs.jsonb.jaxrs.JsonbJaxrsProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.V4Pact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import de.openknowledge.sample.customer.domain.CustomerNumber;
+
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(providerName = "billing-service")
+public class BillingAddressRepositoryTest {
+
+    private BillingAddressRepository repository;
+
+    @Pact(consumer = "customer-service")
+    public V4Pact getMax(PactDslWithProvider builder) {
+        return builder
+          .given("Three customers")
+            .uponReceiving("GET request for 0815")
+            .path("/billing-addresses/0815")
+            .method("GET")
+          .willRespondWith()
+            .status(200)
+            .body(new PactDslJsonBody()
+                    .stringValue("recipient", "Max Mustermann")
+                    .stringValue("city", "26122 Oldenburg")
+                    .object("street")
+                        .stringValue("name", "Poststr.")
+                        .stringValue("number", "1")
+                    .closeObject())
+            .toPact(V4Pact.class);
+    }
+
+    @Pact(consumer = "customer-service")
+    public V4Pact dontGetMissing(PactDslWithProvider builder) throws IOException {
+        return builder
+          .given("Three customers")
+            .uponReceiving("GET request for 0817")
+            .path("/billing-addresses/0817")
+            .method("GET")
+          .willRespondWith()
+            .status(404)
+            .toPact(V4Pact.class);
+    }
+
+    @Pact(consumer = "customer-service")
+    public V4Pact updateMax(PactDslWithProvider builder) throws IOException {
+        return builder
+          .given("Three customers")
+            .uponReceiving("POST request for 0815")
+            .path("/billing-addresses/0815")
+            .method("POST")
+            .matchHeader("Content-Type", "application/json.*", "application/json")
+            .body(new PactDslJsonBody()
+                    .stringValue("recipient", "Erika Mustermann")
+                    .stringValue("city", "45127 Essen")
+                    .object("street")
+                        .stringValue("name", "II. Hagen")
+                        .stringValue("number", "7")
+                    .closeObject())
+          .willRespondWith()
+            .status(200)
+            .toPact(V4Pact.class);
+    }
+
+    @BeforeEach
+    public void initializeRepository(MockServer mockServer) {
+        repository = new BillingAddressRepository();
+        repository.billingServiceUrl = "http://localhost:" + mockServer.getPort();
+        repository.client = ClientBuilder.newClient().register(JsonbJaxrsProvider.class);
+    }
+
+    @PactTestFor(pactMethod = "getMax")
+    @Test
+    public void findBillingAddressForExistingCustomer() {
+        Optional<Address> address = repository.find(new CustomerNumber("0815"));
+        assertThat(address).isPresent().contains(
+            new Address(
+                new Recipient("Max Mustermann"),
+                new Street(new StreetName("Poststr."), new HouseNumber("1")),
+                new City("26122 Oldenburg")));
+    }
+
+    @PactTestFor(pactMethod = "dontGetMissing")
+    @Test
+    public void dontFindNonExistingAddress() {
+        Optional<Address> address = repository.find(new CustomerNumber("0817"));
+        assertThat(address).isNotPresent();
+    }
+
+    @PactTestFor(pactMethod = "updateMax")
+    @Test
+    public void updateAddress() {
+        repository.update(new CustomerNumber("0815"), new Address(
+            new Recipient("Erika Mustermann"),
+            new Street(new StreetName("II. Hagen"), new HouseNumber("7")),
+            new City("45127 Essen")));
+    }
+}

delivery-service/src/main/java/de/openknowledge/sample/address/domain/Address.java
@@ -15,18 +15,38 @@
  */
 package de.openknowledge.sample.address.domain;
 
-import static java.util.Optional.ofNullable;
 import static org.apache.commons.lang3.Validate.notNull;
-
-import java.util.Objects;
 
 import jakarta.json.bind.annotation.JsonbCreator;
 import jakarta.json.bind.annotation.JsonbProperty;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "ADDRESSES")
 public class Address {
+
+    @EmbeddedId
+    CustomerNumber id;
+    @Embedded
     private Recipient recipient;
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "name.name", column = @Column(name = "STREET")),
+        @AttributeOverride(name = "number.number", column = @Column(name = "HOUSE_NUMBER")),
+    })
     private Street street;
+    @Embedded
     private City city;
+
+    protected Address() {
+        // for JPA
+    }
 
     @JsonbCreator
     public Address(@JsonbProperty("recipient") Recipient recipient) {
@@ -57,50 +77,5 @@ public class Address {
 
     public void setCity(City city) {
         this.city = city;
-    }
-
-    @Override
-    public int hashCode() {
-        return recipient.hashCode() + ofNullable(street).map(Object::hashCode).orElse(0) + ofNullable(city).map(Object::hashCode).orElse(0);
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || !getClass().equals(object.getClass())) {
-            return false;
-        }
-        Address address = (Address)object;
-        return recipient.equals(recipient) && Objects.equals(street, address.street) && Objects.equals(city, address.city);
-    }
-
-    public static Builder of(String recipient) {
-        return new Builder(new Recipient(recipient));
-    }
-
-    public static class Builder {
-
-        private Address address;
-
-        private Builder(Recipient recipient) {
-            address = new Address(recipient);
-        }
-
-        public Builder atStreet(String name) {
-            AddressLine addressLine = new AddressLine(name);
-            address.setStreet(new Street(addressLine.getStreetName(), addressLine.getHouseNumber()));
-            return this;
-        }
-
-        public Builder inCity(String city) {
-            address.setCity(new City(city));
-            return this;
-        }
-
-        public Address build() {
-            return address;
-        }
     }
 }

delivery-service/src/main/java/de/openknowledge/sample/address/domain/CustomerNumber.java
@@ -17,14 +17,20 @@ package de.openknowledge.sample.address.domain;
 
 import static org.apache.commons.lang3.Validate.notBlank;
 
+import java.io.Serializable;
+
 import jakarta.json.bind.adapter.JsonbAdapter;
 import jakarta.json.bind.annotation.JsonbTypeAdapter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 
 import de.openknowledge.sample.address.domain.CustomerNumber.Adapter;
 
+@Embeddable
 @JsonbTypeAdapter(Adapter.class)
-public class CustomerNumber {
+public class CustomerNumber implements Serializable {
 
+    @Column(name = "ID")
     private String number;
 
     protected CustomerNumber() {

delivery-service/src/main/java/de/openknowledge/sample/address/domain/StreetName.java
@@ -19,9 +19,11 @@ import static org.apache.commons.lang3.Validate.notBlank;
 
 import jakarta.json.bind.adapter.JsonbAdapter;
 import jakarta.json.bind.annotation.JsonbTypeAdapter;
+import jakarta.persistence.Embeddable;
 
 import de.openknowledge.sample.address.domain.StreetName.Adapter;
 
+@Embeddable
 @JsonbTypeAdapter(Adapter.class)
 public class StreetName {
 